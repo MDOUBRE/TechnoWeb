a2ac8f08d7b48675eb02ea53e8c9d7a6bec02a13
TantePanique <maxime.doubre@gmail.com>
2019-12-30 16:31:55 +0100

    commit du dossier

---
+++ TechnoWeb/src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        Liste list=new Liste();
+        Element elem=new Element();
+        list.ajouteElement(elem);
+        
+    }
+}
