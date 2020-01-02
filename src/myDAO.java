b0de8892b4ebc9f5f659f93a0bff73f1ef9b7db8
TantePanique <maxime.doubre@gmail.com>
2020-01-02 16:23:05 +0100

    correstions elements dans le dao

--- src/myDAO.java
+++ src/myDAO.java
@@ -55,9 +55,6 @@ public class myDAO {
     }
 
 
-
-
-
     public List<Liste> getListeElement(){
       String sql =
         "SELECT * from ELEMENT;";
@@ -67,48 +64,48 @@ public class myDAO {
       }
     }
 
-    public List<Liste> getTitreElement(){
+    public List<Element> getTitreElement(){
       String sql =
         "SELECT TITRE from ELEMENT;";
 
       try(Connection con = sql2o.open()) {
-        return con.createQuery(sql).executeAndFetch(Task.class);
+        return con.createQuery(sql).executeAndFetch(Element.class);
       }
     }
 
-    public List<Liste> getDescriptionElement(){
+    public List<Element> getDescriptionElement(){
       String sql =
         "SELECT DESCRIPTION from ELEMENT;";
 
       try(Connection con = sql2o.open()) {
-        return con.createQuery(sql).executeAndFetch(Task.class);
+        return con.createQuery(sql).executeAndFetch(Element.class);
       }
     }
 
-    public List<Liste> getIdElement(){
+    public List<Element> getIdElement(){
       String sql =
         "SELECT ID from ELEMENT;";
 
       try(Connection con = sql2o.open()) {
-        return con.createQuery(sql).executeAndFetch(Task.class);
+        return con.createQuery(sql).executeAndFetch(Element.class);
       }
     }
 
-    public List<Liste> getIdListeElement(){
+    public List<Element> getIdListeElement(){
       String sql =
         "SELECT IDLISTE from ELEMENT;";
 
       try(Connection con = sql2o.open()) {
-        return con.createQuery(sql).executeAndFetch(Task.class);
+        return con.createQuery(sql).executeAndFetch(Element.class);
       }
     }
 
-    public List<Liste> getDateCreationElement(){
+    public List<Element> getDateCreationElement(){
       String sql =
         "SELECT DATECREATION from ELEMENT;";
 
       try(Connection con = sql2o.open()) {
-        return con.createQuery(sql).executeAndFetch(Task.class);
+        return con.createQuery(sql).executeAndFetch(Element.class);
       }
     }
 
